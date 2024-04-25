13a5bb832dabc8b1f6881d581516761d735d5958
Raffael Dwyyana <raffael.dwyyana@univ-tlse3.fr>
2024-04-25 11:22:57 +0200

    fizzbuzz(2) retourne 2

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -2,6 +2,6 @@ package fizzbuzz;
 
 public class FizzBuzz {
 	public static String fizzbuzz(int i) {
-		return "1";
+		return "" + i;
 	}
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -7,8 +7,13 @@ import org.junit.jupiter.api.Test;
 class FizzBuzzTest {
 
 	@Test
-	void test() {
+	void fizz_1() {
 		assertEquals(FizzBuzz.fizzbuzz(1), "1");
+	}
+	
+	@Test
+	void fizz_2() {
+		assertEquals(FizzBuzz.fizzbuzz(2), "2");
 	}
 
 }
